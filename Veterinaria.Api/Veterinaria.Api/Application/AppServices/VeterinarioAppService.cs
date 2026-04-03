531f2ce8d4f00f5dac485b199417ca03b2fe57ed
KeilyMachado <neyzerorellana66@gmail.com>
2026-04-03 15:58:25 -0600

    Agrego AppService y DomainService de Veterinarios

---
+++ Veterinaria.Api/Veterinaria.Api/Application/AppServices/VeterinarioAppService.cs
@@ -0,0 +1,30 @@
+﻿using Veterinaria.Api.Domain.Entities;
+using Veterinaria.Api.Domain.Interfaces;
+
+namespace Veterinaria.Api.Application.AppServices
+{
+    public class VeterinarioAppService
+    {
+        private readonly IVeterinarioRepository _repo;
+
+        public VeterinarioAppService(IVeterinarioRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<List<Veterinario>> GetAllAsync()
+            => await _repo.GetAllAsync();
+
+        public async Task<Veterinario?> GetByIdAsync(int id)
+            => await _repo.GetByIdAsync(id);
+
+        public async Task AddAsync(Veterinario veterinario)
+            => await _repo.AddAsync(veterinario);
+
+        public async Task UpdateAsync(Veterinario veterinario)
+            => await _repo.UpdateAsync(veterinario);
+
+        public async Task DeleteAsync(int id)
+            => await _repo.DeleteAsync(id);
+    }
+}
--- Veterinaria.Api/Veterinaria.Api/obj/Debug/net10.0/Veterinaria.Api.AssemblyInfo.cs
+++ Veterinaria.Api/Veterinaria.Api/obj/Debug/net10.0/Veterinaria.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Veterinaria.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc88c21a78527514994d729fbb292a9e17242c3c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+845a4d7c20955cd56f39a2a8dd74b1380c0ddc53")]
 [assembly: System.Reflection.AssemblyProductAttribute("Veterinaria.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Veterinaria.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
